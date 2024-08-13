1574386fff40722a353a510e33a0b72c94fdb2b3
Vakarva <151787968+Vakarva@users.noreply.github.com>
2024-08-12 17:05:35 -0700

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Vahe Vartan on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
